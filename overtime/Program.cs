30f399f7b36642d14c684c3683049cb473e5cc40
culexgdubs <d4n13lb3rry@gmail.com>
2021-12-07 13:40:57 -0500

    ot done

--- overtime/Program.cs
+++ overtime/Program.cs
@@ -6,7 +6,22 @@ namespace overtime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Program A = new Program();
+            System.Console.WriteLine(A.OT(new double[]{9, 19, 30, 1.5}));
+        }
+
+        public string OT(double[] a)
+        {
+            double hours = a[1] - a[0];
+            double overT = 0;
+            if(a[1] > 17)
+            {
+                overT = (a[1] - 17) * (a[3] - 1) * a[2];
+            }
+
+            double pay = hours * a[2] + overT;
+            return "$" + pay.ToString("N2");
         }
     }
 }
